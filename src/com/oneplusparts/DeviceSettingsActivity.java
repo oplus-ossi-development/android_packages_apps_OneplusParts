a38c7668ec7f04e35c30b8779121d532c089e4e1
lahaina <nattolecats@gmail.com>
2024-03-24 19:35:25 +0900

    Revert "RealmeParts: Fix build after Android-13.0.0_r18" * After QPR2 it makes errors
    This reverts commit 470c4ac38210618d27883d757e0364e8fc007707.

--- src/com/oneplusparts/DeviceSettingsActivity.java
+++ src/com/oneplusparts/DeviceSettingsActivity.java
@@ -21,7 +21,7 @@ import android.app.Fragment;
 import android.os.Bundle;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.widget.R;
+import com.android.settingslib.collapsingtoolbar.R;
 
 public class DeviceSettingsActivity extends CollapsingToolbarBaseActivity {
 
